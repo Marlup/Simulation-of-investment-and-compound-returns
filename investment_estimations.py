26a23652154f555d2e93ad4c4cc3b17d498fb279
Marlo <marsunlop@gmail.com>
2023-09-26 16:51:01 +0200

    typo corrections; rename

--- investment_estimations.py
+++ investment_estimations.py
@@ -8,14 +8,19 @@ import scipy
 DEFAULT_RETIREMENT_YEARS = 30
 MONTHS_IN_YEAR = 12
 
-def calculate_accumulated_return(principal, yearly_contributions, annual_roi, years, compounding_frequency, return_acum_roi=False):
+def calculate_accumulated_return(principal, 
+                                 annual_roi,
+                                 compounding_frequency,
+                                 annual_contribution, 
+                                 investment_duration, 
+                                 return_acum_roi=False):
     # Calculate the ROI for each compounding period
     term_roi = annual_roi / compounding_frequency
     # Calculate the periodic contribution
-    periodic_contribution = yearly_contributions / compounding_frequency
+    periodic_contribution = annual_contribution / compounding_frequency
 
     # Calculate the accumulated ROI over the specified number of years
-    accumulated_roi = (1 + term_roi) ** (years * compounding_frequency)
+    accumulated_roi = (1 + term_roi) ** (investment_duration * compounding_frequency)
 
     # Calculate the principal amount after the specified number of years
     principal_term = principal * accumulated_roi
@@ -40,63 +45,48 @@ def calculate_return_on_investment(principal,
                                    compounding_frequency=12,
                                    annual_contribution=0.0,
                                    investment_duration=5,
-                                   min_investment_amount=0.0,
-                                   retirement_duration=5,
+                                   retirement_at=0,
                                    retirement_income=0.0,
                                    retirement_contribution=1200,
                                    inflation_rate=0.0,
                                    tax_percentage=0.0):
-
+    if not isinstance(compounding_frequency, int) or (compounding_frequency < 1):
+        compounding_frequency = 1
+    n_compounds = MONTHS_IN_YEAR // compounding_frequency
+    periodic_roi = annual_roi / n_compounds
+    retirement_at_months = MONTHS_IN_YEAR * retirement_at
+    monthly_contribution = annual_contribution / MONTHS_IN_YEAR
+    time_counter = 0
     on_retirement = False
     current_balance = principal
-    earned_interest = 0
-    annual_earnings = []
-
-    if (compounding_frequency is None) or (compounding_frequency < 1):
-        compounding_frequency = 1
-
-    if retirement_income > 0.0 and investment_duration <= retirement_duration:
-        retirement_duration = DEFAULT_RETIREMENT_YEARS
-        investment_duration = 2 * DEFAULT_RETIREMENT_YEARS
-
-    retirement_months = retirement_duration * MONTHS_IN_YEAR
-    monthly_contribution = annual_contribution / MONTHS_IN_YEAR
-    monthly_roi = annual_roi / compounding_frequency
-    month_counter = 0
-    time_counter = 0
-
+    periodic_earnings = []
     for year in range(investment_duration):
         for month in range(1, MONTHS_IN_YEAR + 1):
-            if month_counter % (12 // compounding_frequency) == 0:
-                interest_earned = current_balance * monthly_roi
-                annual_earnings.append(interest_earned)
-                earned_interest += interest_earned
-
+            if month % compounding_frequency == 0:
+                interest_earned = current_balance * periodic_roi
+                periodic_earnings.append(interest_earned)
+                current_balance += interest_earned
+            
             current_balance += monthly_contribution
-
-            if earned_interest >= min_investment_amount and earned_interest > 0:
-                current_balance += earned_interest
-                earned_interest = 0.0
-
-            if on_retirement or time_counter >= retirement_months:
-                if not on_retirement:
-                    on_retirement = True
-                    monthly_contribution = retirement_contribution / MONTHS_IN_YEAR
-                current_balance -= retirement_income
+            
+            if not on_retirement and retirement_at_months > 0:
+                if time_counter >= retirement_at_months:
+                    if not on_retirement:
+                        on_retirement = True
+                        monthly_contribution = retirement_contribution / MONTHS_IN_YEAR
+                else:
+                    time_counter += 1
             else:
-                time_counter += 1
-
-            month_counter += 1
-
+                current_balance -= retirement_income
+        
         if inflation_rate != 0.0:
             current_balance = adjust_for_inflation(current_balance, inflation_rate)
 
         if tax_percentage != 0.0:
-            yearly_earnings = sum(annual_earnings[-MONTHS_IN_YEAR:])
+            yearly_earnings = sum(periodic_earnings[-n_compounds:])
             current_balance -= yearly_earnings * tax_percentage
 
-    return current_balance, annual_earnings
-
+    return current_balance, periodic_earnings
 def define_scenario(initial_amounts,
                     rois,
                     years,
@@ -136,13 +126,12 @@ def define_scenario(initial_amounts,
 
     return accumulated_money
 
-
 def build_dataframe(data):
-  index = pd.MultiIndex.from_tuples(list(data.keys()))
-  df = pd.DataFrame(list(data.values()), index=index)
+    index = pd.MultiIndex.from_tuples(list(data.keys()))
+    df = pd.DataFrame(list(data.values()), index=index)
 
-  df = df.reset_index()
-  df.columns = ['initial_amount', 
+    df = df.reset_index()
+    df.columns = ['initial_amount', 
                 'roi', 
                 'term', 
                 'contrib', 
@@ -150,4 +139,4 @@ def build_dataframe(data):
                 'year', 
                 'retir_income', 
                 'amount']
-  return df
+    return df
